14cce7a808fc1a97b4a4483f083f677ef77fa73c
Leron Does Gaming <leronhinds@gmail.com>
2017-07-17 18:01:50 -0400

    Added project

---
+++ src/ZtechNetwork/Minecraft/AutoServerRestart/CallbackTask.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+namespace ZtechNetwork\Minecraft\AutoServerRestart;
+
+use pocketmine\scheduler\Task;
+
+class CallbackTask extends Task{
+
+	/** @var callable */
+	protected $callable;
+
+	/** @var array */
+	protected $args;
+
+	/**
+	 * @param callable $callable
+	 * @param array    $args
+	 */
+	public function __construct(callable $callable, array $args = array()){
+		$this->callable = $callable;
+		$this->args = $args;
+		$this->args[] = $this;
+	}
+
+	/**
+	 * @return callable
+	 */
+	public function getCallable(){
+		return $this->callable;
+	}
+
+	public function onRun($currentTicks){
+		call_user_func_array($this->callable, $this->args);
+	}
+
+}
